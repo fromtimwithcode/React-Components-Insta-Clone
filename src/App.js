e619e13deb2447bd4e28bc3f89aea8a7e60469d9
Tim Marchant <fromtimwithcode@gmail.com>
2021-08-10 18:59:30 -0500

    Added likes updater

--- src/App.js
+++ src/App.js
@@ -21,6 +21,7 @@ const App = () => {
   // This state is the source of truth for the data inside the app. You won't be needing dummyData anymore.
   // To make the search bar work (which is stretch) we'd need another state to hold the search term.
   const [posts, setPosts] = useState(dummyData);
+  const [searchTerm, setSearchTerm] = useState('');
   const likePost = postId => {
     /*
       This function serves the purpose of increasing the number of likes by one, of the post with a given id.
@@ -33,12 +34,22 @@ const App = () => {
         - if the `id` of the post matches `postId`, return a new post object with the desired values (use the spread operator).
         - otherwise just return the post object unchanged.
      */
+    const updatedLikes = posts.map(item => {
+      if (item.id === postId) {
+        return {...item, likes: (item.likes + 1)};
+      }
+      return item;
+    })
+
+    setPosts(updatedLikes);
   };
 
   return (
     <div className='App'>
+      {/* Add SearchBar and Posts here to render them */}
+      {/* Check the implementation of each component, to see what props they require, if any! */}
       {<SearchBar />}
-      {<Posts />}
+      {<Posts likePost={likePost} posts={posts} />}
     </div>
   );
 };
